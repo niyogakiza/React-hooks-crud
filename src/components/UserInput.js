b21d63afa6055770b3fc901f9f63a59f8b3ba359
Aimable <niyoai@yahoo.fr>
2019-08-05 09:36:03 +0100

    Edit user form

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,61 +1,65 @@
-import React, { useState } from 'react'
-import UserTable from './UserTable';
-import usersData from '../data/usersData'
-import AddUserForm from './AddUserForm'
+import React, { useState, Fragment } from "react";
+import UserTable from "./UserTable";
+import usersData from "../data/usersData";
+import AddUserForm from "./AddUserForm";
+import EditUserForm from "./EditUserForm";
 
 const UserInput = () => {
-  const initialBookState = {
-    title: '',
-    available: false
-  }
+  const initialFormState = { id: null, name: "", username: "" };
 
-  const initialFormState = { id: null, name: '', username: '' }
-
-  const [book, setBook] = useState(initialBookState)
-  const [users, setUsers] = useState(usersData)
-  const [editing, setEditing] = useState(false)
-  const [currentUser, setCurrentUser] = useState(initialFormState)
-
-  const updateBook = book => {
-    setBook({ title: book.title, available: book.available })
-  }
+  const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
 
   const addUser = user => {
-    user.id = users.length + 1
-    setUsers([...users, user])
-  }
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
 
   const deleteUser = id => {
-    setUsers(users.filter(user => user.id !== id))
-  }
+    setUsers(users.filter(user => user.id !== id));
+  };
 
   const editRow = user => {
-    setEditing(true)
-    setCurrentUser({ id: user.id, name: user.name, username: user.username })
-  }
+    setEditing(true);
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
 
   const updateUser = (id, updatedUser) => {
-    setEditing(false)
-    setUsers(users.map(user => (user.id === id ? updateUser : user)))
-  }
-
+    setEditing(false);
+    setUsers(users.map(user => (user.id === id ? updatedUser : user)));
+  };
 
   return (
-    <div className='container'>
-    <h1>Users</h1>
+    <div className="container">
+      <h1>Users</h1>
 
-    <div className='flex-row'>
-    <div className='flex-large'>
-       <h2>Add user</h2>
-       <AddUserForm addUser={addUser} updateUser={updateUser} />
+      <div className="flex-row">
+        <div className="flex-large">
+          {editing ? (
+            <Fragment>
+              <h2>Edit user</h2>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updatedUser={updateUser}
+              />
+            </Fragment>
+          ) : (
+            <Fragment>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </Fragment>
+          )}
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} deleteUser={deleteUser} editRow={editRow} />
+        </div>
+      </div>
     </div>
-    <div className='flex-large'>
-       <h2>View users</h2>
-       <UserTable users={users} deleteUser={deleteUser} editRow={editRow} />
-    </div>
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default UserInput
+export default UserInput;
